a61c05eb3b4180cab6963e1946c6f9abdbd51b17
RyanViers <ryanviersiv@hotmail.com>
2022-06-17 19:25:52 -0400

    fix

--- src/App.js
+++ src/App.js
@@ -109,13 +109,8 @@ class App extends Component {
         <ResponsiveContainer height={400}>
           <ScatterChart margin={{ top: 20, right: 20, bottom: 10, left: 10 }}>
             <CartesianGrid strokeDasharray="3 3" />
-            <XAxis type="category" dataKey="city" name="city" />
-            <YAxis
-              type="number"
-              dataKey="number"
-              name="number of events"
-              allowDecimals={false}
-            />
+            <XAxis type="category" dataKey="city" name="City" />
+            <YAxis type="number" dataKey="number" name="Number of Events" />
             <Tooltip cursor={{ strokeDasharray: '3 3' }} />
             <Scatter data={this.getData()} fill="#8884d8" />
           </ScatterChart>
